31218c3b035ff84d00dfd04e19264d538f87158d
Andres Morales <moralesandres@outlook.com>
2024-03-01 15:20:43 -0500

    3.8

--- agenda2/src/components/Busqueda.jsx
+++
@@ -1,8 +0,0 @@
-
-const Busqueda = (persons, filter) => {
-    return persons.filter(
-      person => person.name.toLowerCase().includes(filter.toLowerCase())
-    );
-  }
-
-export default Busqueda;
